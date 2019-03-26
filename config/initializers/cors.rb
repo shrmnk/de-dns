6dad61a2480576936ffbbf2ee8b91c1a9fb1cb1a
Sherman K <shrmnk@users.noreply.github.com>
2019-03-27 04:50:47 +0800

    Fix devise JWT authentication

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,7 +15,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     
     resource '/api/*',
       headers: %w(Authorization),
-      methods: [:get, :post, :put, :patch, :delete],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
       expose: %w(Authorization),
       max_age: 600
   end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,6 +19,9 @@ Devise.setup do |config|
       ['DELETE', %r{^/logout$}]
     ]
     jwt.expiration_time = 1.day.to_i
+    jwt.request_formats = {
+      user: [:json]
+    }
   end
 
   # ==> Controller configuration
