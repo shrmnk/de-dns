f3f7e9fee44217d39625a53e45ed00f5958b4442
Sherman K <shrmnk@users.noreply.github.com>
2019-03-27 00:16:01 +0800

    Install gems

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,11 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
+# require "action_view/railtie"
+# require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -26,6 +26,30 @@ module DeDns
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    config.time_zone = 'Asia/Singapore'
+
+    config.generators do |g|
+
+      # Use RSpec as test framework
+      g.test_framework :rspec
+      g.helper_specs false
+      g.controller_specs true
+      g.view_specs false
+      g.routing_specs true
+      g.request_specs false
+
+      # Skip helpers
+      g.helper false
+
+      g.assets false
+
+      # Don't generate system test files.
+      g.system_tests nil
+
+    end
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
