f9a7d8ca3eec54585a1ce56903f664a2d08a9ce0
Sherman K <shrmnk@users.noreply.github.com>
2019-03-27 22:36:00 +0800

    Add CloudflareAccounts#create

--- app/controllers/cloudflare_accounts_controller.rb
+++ app/controllers/cloudflare_accounts_controller.rb
@@ -11,4 +11,22 @@ class CloudflareAccountsController < ApplicationController
                           .find(params[:id])
   end
 
+  def create
+    @cloudflare_account = current_user.cloudflare_accounts.new(
+      cloudflare_account_params
+    )
+
+    if @cloudflare_account.save
+      render :show
+    else
+      validation_error(@cloudflare_account)
+    end
+  end
+
+  private
+
+  def cloudflare_account_params
+    params.require(:cloudflare_account).permit(:name, :email, :key)
+  end
+
 end
