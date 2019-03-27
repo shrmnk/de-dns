be62e967b8731693d46c21dad674af2ad64ec535
Sherman K <shrmnk@users.noreply.github.com>
2019-03-27 21:49:58 +0800

    Add CloudflareAccount.cloudflare_records

--- app/models/cloudflare_account.rb
+++ app/models/cloudflare_account.rb
@@ -1,3 +1,5 @@
+require 'cloudflare'
+
 class CloudflareAccount < ApplicationRecord
   belongs_to :user, inverse_of: :cloudflare_accounts
 
@@ -9,6 +11,21 @@ class CloudflareAccount < ApplicationRecord
 
   def display_name
     name || email
+  end
+
+  def cloudflare_zones
+    @cloudflare_zones ||= retrieve_cloudflare_zones
+  end
+
+  private
+
+  def retrieve_cloudflare_zones
+    results = nil
+    Cloudflare.connect(key: key, email: email) do |connection|
+      results = connection.zones.map(&:value)
+    end
+    # Convert to name => id
+    results.map { |z| [z[:name], z[:id]] }.to_h
   end
 
 end
